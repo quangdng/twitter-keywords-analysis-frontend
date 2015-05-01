c713f66b5d257e56717a269dd1c44e732454db91
Thiên Phong <thienphong@ua-unistudent-ten-9-171-187.uniaccess.unimelb.edu.au>
2015-05-01 15:55:50 +1000

    Moved 'fortune cookie' functionality into module.

--- web.js
+++ web.js
@@ -3,6 +3,7 @@
  */
 
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -19,15 +20,9 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.", "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
 app.get('/about', function(req, res){
     var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 // custom 404 page
 app.use(function(req, res, next){
